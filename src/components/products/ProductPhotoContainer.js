778ba6a9a3286e1304a0f7e395f574780dc1d464
thinhnw <thinhnv0412@gmail.com>
2021-01-10 19:00:03 +0700

    Add photo-switch to ProductPhotoContainer

--- src/components/products/ProductPhotoContainer.js
+++ src/components/products/ProductPhotoContainer.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import './css/ProductPhotoContainer.css';
-import img1 from './images/pro_1_1.jpeg';
 
 export default function ProductPhotoContainer(props) {
 
@@ -9,10 +8,28 @@ export default function ProductPhotoContainer(props) {
     for (let i = 1; i <= 4; ++i) {
         let path = `/images/${name.toLowerCase()}_${i}.jpeg`;
         imgs.push(path) ;
-        console.log(path);
     }
 
     const [ index, setIndex ] = useState(0);
+
+    function handleClick(event) {
+
+        let allClasses = event.target.className;
+        console.log(allClasses);
+        if (allClasses.indexOf("0") > 0) setIndex(0);
+        else if (allClasses.indexOf("1") > 0) setIndex(1);
+        else if (allClasses.indexOf("2") > 0) setIndex(2);
+        else if (allClasses.indexOf("3") > 0) setIndex(3);
+    }
+
+    function handlePrev() {
+
+        setIndex( index === 0 ? 3 : index - 1);
+    }
+    function handleNext() {
+
+        setIndex( (index + 1) >= 4 ? 0 : index + 1);
+    }
 
     return (
         <div className="ProductPhotoContainer">
@@ -20,13 +37,19 @@ export default function ProductPhotoContainer(props) {
                 <div className="ProductPhotoContainer_main-inner">
                     <img src={imgs[index]} alt=""/>
                 </div>
+                <button className="ProductPhoto-prev" onClick={handlePrev}>
+
+                </button>
+                <button className="ProductPhoto-next" onClick={handleNext}>
+
+                </button>
             </div>
             <div className="ProductPhotoContainer_sub">
                 {
                     imgs.map((img, i) => {
                         return (
-                            <div key={i} className="ProductPhotoContainer_sub-item">
-                                <img src={img} alt=""/>
+                            <div key={i} className={"ProductPhotoContainer_sub-item"}>
+                                <img src={img} alt="" className={"ProductPhoto-" + i} onClick={handleClick}/>
                             </div>
                         )
                     })
