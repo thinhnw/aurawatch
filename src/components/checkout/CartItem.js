f563ea573406b65a42be58f38b29f056ad2347e3
thinhnw <thinhnv0412@gmail.com>
2021-01-13 10:59:03 +0700

    Tweak Cart Item

--- src/components/checkout/CartItem.js
+++ src/components/checkout/CartItem.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './css/CartItem.css';
-
+import DeleteSweepOutlinedIcon from '@material-ui/icons/DeleteSweepOutlined';
 export default function CartItem(props) {
 
+    const [ qty, setQty ] = useState(props.qty);
+
+    function handleChange(event, value) {
+        setQty(value)
+    }
     return (
         <div className="CartItem">
             <div className="CartItem_img-wrapper">
@@ -11,9 +16,13 @@ export default function CartItem(props) {
             <div className="CartItem_info">
                 <p><strong>{props.coll}</strong></p>
                 <p>{props.name}</p>
+                <div className="CartItem_price">
+                    <input type="number" name="qty" min="0" max="99" value={qty} onChange={handleChange}/>
+                    <p> x ${props.price}.00</p>
+                </div>
             </div>
-            <div className="CartItem_price">
-                <p>{props.qty} x ${props.price}.00</p>
+            <div className="CartItem_delete">
+                <DeleteSweepOutlinedIcon fontSize="large" className="CartItem_deleteIcon"/>
             </div>
         </div>
     )
